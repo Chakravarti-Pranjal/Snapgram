9df3b82e4a0d1eca2877788b72ae11f46136fe0b
Chakravarti-Pranjal <chakravartipranjal3@gmail.com>
2024-06-01 19:15:07 +0530

    shadcn form initialized

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,9 +9,11 @@ const AuthLayout = () => {
         <Navigate to='/' />
       ) : (
         <>
-            <section>
+            <section className="flex flex-1 justify-center items-center flex-col py-10">
                 <Outlet />
             </section>
+
+            <img src="https://img.freepik.com/premium-vector/social-media-marketing-mobile-style_23-2148410478.jpg?w=740" alt="imgs" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat "  />
         </>
       )
 
